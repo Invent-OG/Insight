e98305b1f2ff00dd49bced2a51dc173ac36de8b1
rahulachuz <rahulachuz69@gmail.com>
2025-08-13 14:47:14 +0530

    layout seo setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import FloatingContactButtons from '@/components/sections/FloatingContactButtons
 import { Toaster } from 'react-hot-toast';
 import { Footer } from '@/components/sections/footer-section';
 import { Parkinsans } from 'next/font/google';
+import Script from 'next/script';
 
 export const metadata = {
   icon: '/assets/logo.png',
@@ -18,10 +19,28 @@ const bokorFont = Parkinsans({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className={bokorFont.className}>
-      <body>
-        <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/favicon.ico' />
+      <head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-
+        <Script id='gtm-script' strategy='afterInteractive'>
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WKPZBCCS');
+  `}
+        </Script>
+      </head>
+      <body>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-WKPZBCCS'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Providers>
           <Toaster />
           <LenisProvider />
