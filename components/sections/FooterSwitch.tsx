d9c42890d06e888a950dea8618a4899213c0c7f8
rahulachuz <rahulachuz69@gmail.com>
2025-08-21 15:28:48 +0530

    Refactor layout and remove unused components; update sitemap with new lastmod dates

--- components/sections/FooterSwitch.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { usePathname } from 'next/navigation';
-import { Footer } from '@/components/sections/footer-section';
-
-export default function FooterSwitch() {
-  const pathname = usePathname();
-  if (pathname?.startsWith('/countries')) return null;
-  return <Footer />;
-}
